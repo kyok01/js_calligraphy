f1415e579a2598f0343834e8a4764c173fdfe4e0
kyok01 <kazutakayokoils@gmail.com>
2022-06-18 01:49:06 +0900

    simple routing done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
-// import ReactDOM from "react-dom";
-import { useState } from "react";
-import { ColLeft } from "./components/colLeft";
-import { ColRight } from "./components/colRight";
+import { Routes, Route, Link, NavLink} from 'react-router-dom'
 import { Cols } from "./components/cols";
+import { About } from "./routes/about";
+import { NoMatch } from "./routes/noMatch";
 
 function App() {
   return (
-    <Cols />
+    <>
+    <ul>
+  <li>
+    <NavLink to="/" className={({ isActive }) => (isActive ? 'menu__active' : undefined)}>Home</NavLink>
+  </li>
+  <li>
+    <NavLink to="/about" className={({ isActive }) => (isActive ? 'menu__active' : undefined)}>About</NavLink>
+  </li>
+</ul>
+    <Routes>
+      <Route path="/" element={<Cols />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NoMatch />} />
+    </Routes>
+    </>
+    
+    
   );
 }
 
---
+++ src/routes/noMatch.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const NoMatch = () => {
+    return <h2>There is no page</h2>
+}
