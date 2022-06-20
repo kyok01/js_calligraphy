17ea2bdc51e703299a5fb193138ecc021083818d
kyok01 <kazutakayokoils@gmail.com>
2022-06-20 18:47:14 +0900

    pageRouter deployed

---
+++ src/pageRouter/pageRouter.jsx
@@ -0,0 +1,45 @@
+import { Routes, Route, Link, NavLink } from "react-router-dom";
+import { Cols } from "../components/templates/cols";
+import { About } from "../components/pages/about";
+import { Art } from "../components/pages/art";
+import { Arts } from "../components/pages/arts";
+import { NoMatch } from "../components/pages/noMatch";
+import { Post } from "../components/pages/post";
+
+export const PageRouter = () => {
+    return (<>
+    <ul>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "menu__active" : undefined
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? "menu__active" : undefined
+            }
+          >
+            About
+          </NavLink>
+        </li>
+      </ul>
+    <Routes>
+        <Route path="/" element={<Cols />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/post" element={<Post />} />
+        <Route path="/arts" element={<Arts />} >
+          <Route path=":artId" element={<Art />} />
+        </Route>
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </>
+        
+    );
+}
