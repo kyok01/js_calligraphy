b7f91b1aff8469ce776a7653b8a976ccd4d71d62
kyok01 <kazutakayokoils@gmail.com>
2022-06-21 10:58:51 +0900

    My page deploying

--- src/components/pages/userMypage.jsx
+++ src/components/pages/userMypage.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { useRecoilValue } from 'recoil';
 import { userState } from '../../store/userState';
 
 export const UserMypage = () => {
+    const { lid } = useParams();
     const navigate = useNavigate();
     const userInfo = useRecoilValue(userState);
     useEffect(() => {
-        !userInfo.isLogin && navigate("/login");
+        // !userInfo.isLogin && userInfo.lid === lid && navigate("/login");
+        if(!userInfo.isLogin && userInfo.lid !== lid){
+            navigate("/login")
+        }
       }, [userInfo]);
   return (
+    <>
     <div>userMypage</div>
+    <p>Welcome! {userInfo.lid}</p>
+    </>
   )
 }
