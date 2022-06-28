36837e69fa98b43a0ac037b069c00ef55946af22
kyok01 <kazutakayokoils@gmail.com>
2022-06-28 11:07:18 +0900

    temporary saved

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/cc-ver1/">
+  <BrowserRouter basename="/cc-ver2/">
     <App />
   </BrowserRouter>
 );
